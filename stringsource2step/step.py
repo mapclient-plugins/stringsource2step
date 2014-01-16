ce0c50d03d8e96ced2b8e370f2bfa44e46d701d0
Alex Carleton <acar246@bioeng242.bioeng.auckland.ac.nz>
2014-01-16 15:09:32 +1300

    initalise

---
+++ stringsource2step/step.py
@@ -0,0 +1,149 @@
+
+'''
+MAP Client Plugin Step
+'''
+import os
+
+from PySide import QtGui
+from PySide import QtCore
+
+from mountpoints.workflowstep import WorkflowStepMountPoint
+from stringsource2step.configuredialog import ConfigureDialog
+
+
+class StringSource2Step(WorkflowStepMountPoint):
+    '''
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    '''
+
+    def __init__(self, location):
+        super(StringSource2Step, self).__init__('String Source 2', location)
+        self._configured = False # A step cannot be executed until it has been configured.
+        self._category = 'General'
+        # Add any other initialisation code here:
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'string'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'tuple'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'dictionary'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      'string'))
+        self._config = {}
+        self._config['identifier'] = ''
+        self._config['string'] = ' '
+
+
+    def execute(self):
+        '''
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        '''
+        # Put your execute step code here before calling the '_doneExecution' method.
+        if len(self.string) != 0:
+            self.stringOut = self._config['string'] % (self.string)
+            print self.stringOut
+            self._doneExecution()
+        elif len(self.Tuple) != 0:
+            self.stringOut = self._config['string'] % self.Tuple
+            self._doneExecution() 
+        else:
+            self.stringOut = self._config['string'] % self.dictionary
+            self._doneExecution()
+
+    def setPortData(self, index, dataIn):
+        '''
+        Add your code here that will set the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        uses port for this step then the index can be ignored.
+        '''
+        if index == 0:
+            self.string = dataIn # string
+        elif index == 1:
+            self.Tuple = dataIn # list
+        else:
+            self.dictionary = dataIn # dictionary
+
+    def getPortData(self, index):
+        '''
+        Add your code here that will return the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        provides port for this step then the index can be ignored.
+        '''
+        return self.stringOut # string
+
+    def configure(self):
+        '''
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        '''
+        dlg = ConfigureDialog()
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+        
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+        
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        '''
+        The identifier is a string that must be unique within a workflow.
+        '''
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        '''
+        The framework will set the identifier for this step when it is loaded.
+        '''
+        self._config['identifier'] = identifier
+
+    def serialize(self, location):
+        '''
+        Add code to serialize this step to disk.  The filename should
+        use the step identifier (received from getIdentifier()) to keep it
+        unique within the workflow.  The suggested name for the file on
+        disk is:
+            filename = getIdentifier() + '.conf'
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        conf.setValue('identifier', self._config['identifier'])
+        conf.setValue('string', self._config['string'])
+        conf.endGroup()
+
+
+    def deserialize(self, location):
+        '''
+        Add code to deserialize this step from disk.  As with the serialize 
+        method the filename should use the step identifier.  Obviously the 
+        filename used here should be the same as the one used by the
+        serialize method.
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        self._config['identifier'] = conf.value('identifier', '')
+        self._config['string'] = conf.value('string', ' ')
+        conf.endGroup()
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
